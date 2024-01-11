6d087f4ebf337efadeb0ec629af9e771b655c3fb
gokhan <mertgokhandonmez@gmail.com>
2024-01-11 20:36:24 +0300

    base entities created

---
+++ Core/OnionApi.Domain/Entities/Brand.cs
@@ -0,0 +1,16 @@
+﻿using OnionApi.Domain.Common;
+
+namespace OnionApi.Domain.Entities;
+
+public class Brand : EntityBase
+{
+    public Brand()
+    {
+        
+    }
+    public Brand(string name)
+    {
+        Name = name;
+    }
+    public required string Name { get; set; }
+}
